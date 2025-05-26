a6a0fa6e9ef1e6b83a60d4dbd32bd739f476d587
Liljana <liljana.ratushnyak@gmail.com>
2025-05-26 17:25:30 +0200

    Modified: apps/bookings/admin.py

--- apps/bookings/admin.py
+++ apps/bookings/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.bookings.models import Booking
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'property',
+        'tenant',
+        'start_date',
+        'end_date',
+        'status',
+        'created_at'
+    )
+    list_filter = (
+        'status',
+        'created_at',
+        'start_date',
+        'end_date'
+    )
+    search_fields = (
+        'property__title',
+        'tenant__email',
+        'tenant__first_name',
+        'tenant__last_name',
+    )
+    ordering = (
+        '-created_at'
+    )
+    autocomplete_fields = (
+        'property',
+        'tenant'
+    )
